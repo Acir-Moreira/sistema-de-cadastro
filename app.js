f894a2777464a66a4fdd4d9c5c4aaf1721bbc77a
Acir Moreira <acirmoreira@MacBook-Air-de-Acir.local>
2023-02-26 12:55:05 -0300

    Adicionando um outro console.log

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação se conectando ao banco de dados")
 console.log("Enviando mensagem de teste")
+console.log("Enviando para o GITHUB")
