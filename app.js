c79b4bdf664fe22aac0c91cd8bb0b58d10fc4d90
Acir Moreira <acirmoreira@MacBook-Air-de-Acir.local>
2023-02-26 13:04:07 -0300

    Voltando o status anterior

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação se conectando ao banco de dados")
 console.log("Enviando mensagem de teste")
-console.log("Enviando para o GITHUB")
